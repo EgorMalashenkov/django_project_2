a300442dffa790bb3f58970f343359ff73f6ac5b
Egor <egor_malashenkov@mail.ru>
2021-04-07 23:06:55 +0300

    bugfix

--- script.py
+++ script.py
@@ -1,10 +1,18 @@
 from data import jobs, companies, specialties
+import os
+import django
+os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings"
+django.setup()
 from vacancies.models import Company, Vacancy, Specialty
 
+
 if __name__ == "__main__":
-    Company.objects.create(
-        name="ABC",
-        location="spbssss",
-        description="asda",
-        employee_count=2
-                          )
+
+    for jobs_data in jobs:
+        Vacancy.objects.create(**jobs_data)
+
+
+
+
+#if __name__ == "__main__":
+    #Specialty.objects.all().delete()
